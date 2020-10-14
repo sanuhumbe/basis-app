e93627eb9e2a258c69dc36a25252237db5cea27d
Sanika <sanika.humbe@net-mon.net>
2020-10-15 02:39:28 +0530

    Updates

---
+++ precache-manifest.9883f6cfcbb07ac85203feb0d906e7d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32827cfea617d3cd7833c2186c3aac91",
+    "url": "/basis-app/index.html"
+  },
+  {
+    "revision": "ea4edc54df571f6b92ca",
+    "url": "/basis-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95004966f50a53a2a30a",
+    "url": "/basis-app/static/js/2.3d841144.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/basis-app/static/js/2.3d841144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4edc54df571f6b92ca",
+    "url": "/basis-app/static/js/main.3b2450f0.chunk.js"
+  },
+  {
+    "revision": "dde4035b64deb6136b29",
+    "url": "/basis-app/static/js/runtime-main.d6617d2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basis-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
